3d8a689d6e85dfb40348535364c551acce51a0dc
tyabuta <gmforip@gmail.com>
2014-01-20 11:53:30 +0900

    add aタグ置換関数追加

--- jquery-macro.js
+++ jquery-macro.js
@@ -13,3 +13,16 @@ function addInputHidden($to, _name, _value){
     $('<input>').attr({type:'hidden', name:_name, value:_value}).appendTo($to);
 }
 
+/*
+* 全aタグのhrefリンクをonclickによる遷移に置換する
+*/
+function anchorHrefToEventScript(){
+    $('a').each(function(){
+        var url = $(this).attr('href');
+        $(this).removeAttr('href');
+        $(this).click(function(){
+            location.href = url;
+        });
+    });
+}
+
